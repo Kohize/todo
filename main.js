5e54b4e50d3412ff2129f0edb784115fa0bfca40
Kohize <kohi881@gmail.com>
2024-11-02 21:58:42 +0300

    base ui

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './styles/style.scss'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+/* 
+TODO
+Разметка: Колонка слева с разными видами туду. сами туду справа
+Логика создания туду class + Пару моковых
+Удаление туду
+Пометка завершенности туду
+Логика переключения видов туду
+Сохранение туду в LocalStorage
+
+*/
+
 
 setupCounter(document.querySelector('#counter'))
